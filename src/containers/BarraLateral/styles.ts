d23432244886bd07843b950e9a2e545c5f3e1f68
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-09-18 23:40:50 -0300

    criando o estilo para o componente BarraLateral

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+    padding: 8px;
+    background-color: #eee;
+    height: 100vh;
+`
+
+export const Filtro = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 8px;
+    margin-top: 16px;
+`
+export const Campo = styled.input`
+    border-radius: 8px;
+    padding: 8px;
+    background-color: #fff;
+    font-weight: bold;
+    color: #666666;
+    border-color: #666666;
+    width: 100%;
+`
