09ac919ff8f260d046dab58763dda64329fdaec8
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-09-18 23:41:22 -0300

    criando o componente de ListaDeTarefas

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,12 @@
+export const ListaDeTarefas = () => {
+    return (
+        <div>
+            <main>
+                <p>
+                    2 tarefas marcadas como: &quot;categoria&ldquo; e
+                    &quot;termo&ldquo;
+                </p>
+            </main>
+        </div>
+    )
+}
